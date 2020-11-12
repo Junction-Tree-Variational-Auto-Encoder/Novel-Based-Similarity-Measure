a4d4e7537508ce738ed3699b7adbc4d2587bc254
cfalholt <73829958+cfalholt@users.noreply.github.com>
2020-11-12 11:35:21 +0100

    Added pip3 istall torch

--- test.sh
+++ test.sh
@@ -28,6 +28,7 @@
 # here follow the commands you want to execute 
 module load python3
 pip3 install --user --upgrade pip
+pip3 install --user torch torchvision
 module unload python3
 
 module load cuda/10.2
