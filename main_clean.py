b7f668eb3a5018c61794b9898e95861c88cb65c7
cfalholt <73829958+cfalholt@users.noreply.github.com>
2020-12-28 10:53:52 +0100

    Code update

---
+++ main_clean.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import rdkit
+from rdkit import DataStructs, Chem
+from rdkit.Chem import MACCSkeys, Draw
+import torch
+
+inp = pd.read_csv('./data/train.txt', names=['SMILES'])
+
+inp['INCHI'] = inp['SMILES'].apply(lambda x: Chem.MolToInchiKey(Chem.MolFromSmiles(x)))
+inp = inp.drop_duplicates(subset=['INCHI'], keep='first')
+
+from fast_molvae.sample import load_model
+model = load_model('./data/vocab.txt', './fast_molvae/vae_model/model.epoch-19')
+
+out_tensor = model.encode_from_smiles(inp['SMILES'].iloc[0:1])
+out_numpy = out_tensor.cpu().data.numpy()
+out_df = pd.DataFrame(out_numpy)
+
+out_df.to_csv('./latent_space/encoded.txt', index=False)
