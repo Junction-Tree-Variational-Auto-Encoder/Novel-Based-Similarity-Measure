93c7878d1b981c8189b374583663c244b6b1bfcb
TobiasThalbitzer <tobiasawt@gmail.com>
2020-12-29 21:12:20 +0100

    Update cluster_gen_mol_1.sh

--- cluster_gen_mol_1.sh
+++ cluster_gen_mol_1.sh
@@ -36,7 +36,7 @@ module load cuda/10.2
 nvidia-smi
 /appl/cuda/10.2/samples/NVIDIA_CUDA-10.2_Samples/bin/x86_64/linux/release/deviceQuery
 
-pip install torch===1.7.0 torchvision===0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
+pip3 install torch===1.7.0 torchvision===0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
 module load pandas/1.0.3-python-3.7.7
 module load scipy/1.4.1-python-3.7.7
 module load numpy/1.18.2-python-3.7.7-openblas-0.3.9
