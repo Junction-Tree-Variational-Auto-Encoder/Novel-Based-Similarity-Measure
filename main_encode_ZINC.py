0d5985950f5582145c1ee226d6816f2586fa0c28
cfalholt <73829958+cfalholt@users.noreply.github.com>
2020-12-30 09:39:37 +0100

    Update main_encode_ZINC.py

--- main_encode_ZINC.py
+++ main_encode_ZINC.py
@@ -4,15 +4,12 @@ from rdkit import DataStructs, Chem
 from rdkit.Chem import MACCSkeys, Draw
 import torch
 
-inp = pd.read_csv('./data/train.txt', names=['SMILES'])
-
-#inp['INCHI'] = inp['SMILES'].apply(lambda x: Chem.MolToInchiKey(Chem.MolFromSmiles(x)))
-#inp = inp.drop_duplicates(subset=['INCHI'], keep='first')
+data = pd.read_csv('./data/train.txt', names=['SMILES'])
 
 from fast_molvae.sample import load_model
 model = load_model('./data/vocab.txt', './fast_molvae/vae_model/model.epoch-19')
 
-out_tensor = model.encode_from_smiles(inp['SMILES'])
+out_tensor = model.encode_from_smiles(data['SMILES'])
 out_numpy = out_tensor.cpu().data.numpy()
 out_df = pd.DataFrame(out_numpy)
 
