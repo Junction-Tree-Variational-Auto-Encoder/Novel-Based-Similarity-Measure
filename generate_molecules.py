95b1b983441b28903626d77cb440f8fdaccb17b6
cfalholt <73829958+cfalholt@users.noreply.github.com>
2021-01-03 17:53:57 +0100

    Update generate_molecules.py

--- generate_molecules.py
+++ generate_molecules.py
@@ -12,21 +12,21 @@ data = pd.read_csv('./data/train.txt', names=['SMILES'])
 from fast_molvae.sample import load_model
 model = load_model('./data/vocab.txt', './fast_molvae/vae_model/model.epoch-19').cuda()
 
-out_vecs, out_mean, out_var = model.encode_test(data['SMILES'][:5])
+out_latent = model.encode_to_latent(data['SMILES'][3:4])
 
 smiles = []
 latent_space = []
 
 
-for i in range(1):
+for i in range(10):
     noise = np.random.normal(1,0.1, 56)
     noise = np.expand_dims(noise,axis = 0)
     noise = torch.from_numpy(noise).float().cuda()
-    out_mean_noise= out_mean*noise
+    out_latent_noise = out_latent*noise
 
-    z_t = out_mean_noise[0:1,0:28].cuda()
-    z_mol = out_mean_noise[0:1,28:56].cuda()
-    latent_space.append(out_mean_noise)
+    z_t = out_latent_noise[0:1,0:28].cuda()
+    z_mol = out_latent_noise[0:1,28:56].cuda()
+    latent_space.append(out_latent_noise)
     smiles.append(model.decode(z_t, z_mol, False))
 
 
