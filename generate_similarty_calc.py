5cbd2d2a6245c2f753a8f09d5423e4d8feaa7fb2
cfalholt <73829958+cfalholt@users.noreply.github.com>
2021-01-04 15:24:28 +0100

    Update

--- generate_similarty_calc.py
+++ generate_similarty_calc.py
@@ -10,8 +10,7 @@ data_tan_eu = pd.read_csv('./latent_space/data_smiles_with_tanimoto_and_euclidia
 
 
 
-data_p_vals = pd.read_csv('./latent_space/logP_values.txt').drop(columns={'Unnamed: 0'}).rename(columns= {'0':'logp'})
-
+data_p_vals = pd.read_csv('./latent_space/logP_values.txt').drop(columns={'Unnamed: 0'})
 import matplotlib.pyplot as plt
 fig = plt.figure()
 plt.hist(data_p_vals['logp'], bins= 100)
