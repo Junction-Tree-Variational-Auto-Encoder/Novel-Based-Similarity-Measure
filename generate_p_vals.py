6d860485e296bb045a4bb3f317c0f34d13c003f3
cfalholt <73829958+cfalholt@users.noreply.github.com>
2021-01-04 14:47:36 +0100

    Update

--- generate_p_vals.py
+++ generate_p_vals.py
@@ -6,7 +6,7 @@ import torch
 import numpy as np 
 
 
-data_tan_eu = pd.read_csv('./latent_space/shaakabraah/data_smiles_with_tanimoto_and_euclidian.txt').drop(columns={'Unnamed: 0'})
+data_tan_eu = pd.read_csv('./latent_space/data_smiles_with_tanimoto_and_euclidian.txt').drop(columns={'Unnamed: 0'})
 data_p_vals = pd.read_csv('./latent_space/P_values.txt').drop(columns={'Unnamed: 0'}).rename(columns= {'0':'P_values'})
 
 
@@ -18,19 +18,18 @@ data_p_vals['Euclidean_distance'] = data_tan_eu['Euclidian_distance']
 
 #
 cut_off_value = 0.50
-orig_mol =  data_p_vals.iloc[3,1]
+orig_mol_logP =  data_p_vals.iloc[3,1]
 
 #
-data_p_vals['cut_off'] =np.abs(data_p_vals['logp'] -  orig_mol)
+data_p_vals['cut_off'] =np.abs(data_p_vals['logp'] -  orig_mol_logP)
 data_p_vals['novel_sim'] = data_p_vals['Euclidean_distance']
 data_p_vals['novel_sim'].loc[data_p_vals['cut_off'] > cut_off_value] = 0
 
 import matplotlib.pyplot as plt
-
-##plt.scatter(np.arange(0,len(data_p_vals)), data_p_vals['logp'], s = 1, label = 'values within %')
-#plt.plot(cut_off_value)
-plt.hist(data_p_cut['logp'], bins= 100)
-plt.title('Cut off')
-plt.ylabel('e')
-plt.xlabel('Molecules')
+fig = plt.figure()
+plt.hist(data_p_vals['logp'], bins= 100)
+plt.ylabel('Molecules')
+plt.xlabel('log p(x)')
+plt.xlim([3,8])
 plt.legend()
+fig.savefig('./')
