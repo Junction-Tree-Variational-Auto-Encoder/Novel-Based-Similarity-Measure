a5c937f1dbd2c4193fab78c78c9ef55ed76975bb
cfalholt <73829958+cfalholt@users.noreply.github.com>
2021-01-04 14:59:33 +0100

    Update

--- generate_similarty_calc.py
+++ generate_similarty_calc.py
@@ -7,10 +7,22 @@ import numpy as np
 
 
 data_tan_eu = pd.read_csv('./latent_space/data_smiles_with_tanimoto_and_euclidian.txt').drop(columns={'Unnamed: 0'})
+
+
+
 data_p_vals = pd.read_csv('./latent_space/P_values.txt').drop(columns={'Unnamed: 0'}).rename(columns= {'0':'P_values'})
 
 
 data_p_vals['logp'] = np.log(data_p_vals['P_values'])
+
+import matplotlib.pyplot as plt
+fig = plt.figure()
+plt.hist(data_p_vals['logp'], bins= 100)
+plt.ylabel('Molecules')
+plt.xlabel('log p(x)')
+plt.xlim([3,8])
+plt.legend()
+fig.savefig('./sh_and_pics/log_p_his.png')
 
 data_p_vals['Tanimoto'] = data_tan_eu['Tanimoto_Similarity']
 data_p_vals['Euclidean_distance'] = data_tan_eu['Euclidian_distance']
@@ -24,12 +36,3 @@ orig_mol_logP =  data_p_vals.iloc[3,1]
 data_p_vals['cut_off'] =np.abs(data_p_vals['logp'] -  orig_mol_logP)
 data_p_vals['novel_sim'] = data_p_vals['Euclidean_distance']
 data_p_vals['novel_sim'].loc[data_p_vals['cut_off'] > cut_off_value] = 0
-
-import matplotlib.pyplot as plt
-fig = plt.figure()
-plt.hist(data_p_vals['logp'], bins= 100)
-plt.ylabel('Molecules')
-plt.xlabel('log p(x)')
-plt.xlim([3,8])
-plt.legend()
-fig.savefig('./sh_and_pics/log_p_his.png')
